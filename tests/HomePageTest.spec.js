ac7cbd23fb26a5c2412aa51a61c9f06dc2b348a2
adityak <adityak@dewsolutions.in>
2025-03-28 17:02:47 +0530

    second push

--- tests/HomePageTest.spec.js
+++ tests/HomePageTest.spec.js
@@ -1,27 +1,27 @@
-const {test,expect} = require ('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
-test ('Homepage Test', async({page})=>{
+test('Homepage Test', async ({ page }) => {
 
-await page.goto('https://www.demoblaze.com/');
+    await page.goto('https://www.demoblaze.com/');
 
- const pageTitle=await page.title();
+    const pageTitle = await page.title();
 
-console.log('Page Title is: ', pageTitle);
-await expect(page).toHaveTitle('STORE');
+    console.log('Page Title is: ', pageTitle);
+    await expect(page).toHaveTitle('STORE');
 
-await expect(page).toHaveURL('https://www.demoblaze.com/');
+    await expect(page).toHaveURL('https://www.demoblaze.com/');
 
-const pageURL=await page.url();
+    const pageURL = await page.url();
 
-console.log('Page URL is : ',pageURL);
-
-
+    console.log('Page URL is : ', pageURL);
 
 
 
+    //ading comment for the re upload the code to the Git
 
+    console.log("2nd push")
 
-await page.close();
+    await page.close();
 
 
 
--- tests/inputBox.spec.js
+++ tests/inputBox.spec.js
@@ -1,21 +1,23 @@
-const{test,expect}=require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
-test('INput box Test',async({page})=>{
+test('INput box Test', async ({ page }) => {
 
 
     await page.goto('https://www.facebook.com/r.php');
 
 
     //Input Box
-   await expect(await page.locator('//input[@name="firstname"]')).toBeVisible();
-   await expect(await page.locator('//input[@name="firstname"]')).toBeEmpty();
-   await expect(await page.locator('//input[@name="firstname"]')).toBeEditable();
-   await expect(await page.locator('//input[@name="firstname"]')).toBeEnabled();
-await page.locator('//input[@name="firstname"]').fill('Aditya');
+    await expect(await page.locator('//input[@name="firstname"]')).toBeVisible();
+    await expect(await page.locator('//input[@name="firstname"]')).toBeEmpty();
+    await expect(await page.locator('//input[@name="firstname"]')).toBeEditable();
+    await expect(await page.locator('//input[@name="firstname"]')).toBeEnabled();
+    await page.locator('//input[@name="firstname"]').fill('Aditya');
 
-await page.waitForTimeout(5000);
+    await page.waitForTimeout(5000);
 
+    //input box recheck repush to the git
 
+    console.log("second push")
 
 
 })
